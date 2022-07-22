cd6600c4f2c67a887f40988fc7018ffadbd56ea2
Irwin Chen <irwin.t.chen@gmail.com>
2022-07-22 03:20:54 -0700

    Time: 154 ms (32.62%), Space: 49 MB (81.32%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,8 +3,5 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let str = s.join('');
-    for (let i = 0; i < s.length; i++) {
-        s[i] = str[str.length - 1 - i];
-    }
+    s.reverse();
 };
