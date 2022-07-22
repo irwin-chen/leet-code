b8ed2d685d1395f2b9a2752b45a21dadbc867ee5
Irwin Chen <irwin.t.chen@gmail.com>
2022-07-21 18:35:43 -0700

    Time: 67 ms (89.45%), Space: 42.6 MB (37.58%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,10 +3,9 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    const sumList = [nums[0]];
-    
-    for (let i = 1; i < nums.length; i++) {
-        sumList.push(nums[i]+sumList[i-1]);
-    }
-    return sumList;
+    let sum = 0;
+    let arrOfSums = nums.map(x => {
+        return sum += x;
+    })
+    return arrOfSums;
 };
